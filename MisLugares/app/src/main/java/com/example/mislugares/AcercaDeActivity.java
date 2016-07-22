95fce71ea83bbbb64065cfa2a2d7975c320bd7fe
Juan Albarrán <tazjc2505@gmail.com>
2016-07-22 10:38:33 -0500

    edx UPValencia Android Projects

---
+++ MisLugares/app/src/main/java/com/example/mislugares/AcercaDeActivity.java
@@ -0,0 +1,15 @@
+package com.example.mislugares;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by juan.albarran on 07/21/2016.
+ */
+public class AcercaDeActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
